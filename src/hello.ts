e902e75e763cf89b633012c172bf5dcb33b9afcf
Prasanna D <dommalapati.chowdary@gmail.com>
2018-11-23 11:24:37 +0000

    Update hello.ts

--- src/hello.ts
+++ src/hello.ts
@@ -1,7 +1,18 @@
 import * as functions from "firebase-functions";
+import * as cors       from 'cors';
+
+const handleCors = cors({methods: 'POST',origin: true});
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  Promise.resolve()
+
+    return handleCors(request, response, () => hello(request, response));
+
+});
+
+function hello(request, response) {
+  
+   Promise.resolve()
     .then(() => response.send("Hello from firebase!"))
     .catch(() => response.sendStatus(403));
-});
+}
+
